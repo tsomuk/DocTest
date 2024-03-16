9390a693c133cc37114b0a3f26f35a125589349c
Tsomuk Nikita <tsomuk@me.com>
2024-03-16 13:33:51 +0200

    Initial Commit

---
+++ DocTest/DocTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocTestApp.swift
+//  DocTest
+//
+//  Created by Nikita Tsomuk on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
