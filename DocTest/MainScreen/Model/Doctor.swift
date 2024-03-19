dc297c408510124bbfd0f0e086cfeb2dee34f256
Tsomuk Nikita <tsomuk@me.com>
2024-03-19 21:58:29 +0200

    Init

---
+++ DocTest/MainScreen/Model/Doctor.swift
@@ -0,0 +1,11 @@
+//
+//  DoctocModel.swift
+//  DocTest
+//
+//  Created by Nikita Tsomuk on 17.03.2024.
+//
+
+import SwiftUI
+
+
+struct Doctor
---
+++ DocTest/MainScreen/View/Components/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  DocTest
+//
+//  Created by Nikita Tsomuk on 18.03.2024.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
