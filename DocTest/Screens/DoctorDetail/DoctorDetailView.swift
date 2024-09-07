abda426c947df2f0c26a90174f2b8cea1432b66b
Nikita Tsomuk <tsomuk@me.com>
2024-09-07 21:16:08 +0300

    [Clean] Refactor `DoctorDetailView`

--- DocTest/Screens/DoctorDetail/DoctorDetailView.swift
+++ DocTest/Screens/DoctorDetail/DoctorDetailView.swift
@@ -14,61 +14,30 @@ struct DoctorDetailView: View {
     
     var body: some View {
         
-        ZStack(content: {
+        ZStack {
             Color.background.edgesIgnoringSafeArea(.all)
-            VStack(alignment: .leading) {
-                HStack(spacing: 16) {
-                    HStack(alignment: .top) {
-                        AsyncImage(url: URL(string: doctor.avatar ?? "")) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                                    .frame(width: 50, height: 50)
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 50, height: 50)
-                                    .cornerRadius(25)
-                            default:
-                                Image(systemName: "person.circle.fill")
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .foregroundStyle(.accent)
-                            }
-                        }
-                    }
-                    
-                    Text("\(doctor.lastName)\n\(doctor.firstName) \(doctor.patronymic)")
-                        .font(.system(size: 16, weight: .semibold))
-                } 
-                .padding(.horizontal, 16)
+            
+            VStack(alignment: .leading, spacing: 20) {
                 
-                VStack(alignment: .leading, spacing: 10) {
-                    TitleWithImage(title: doctor.seniority.getYearsStringDetail(), imageName: "clock")
-                    TitleWithImage(title: chechScientificDegreeLabel(), imageName: "cross.case")
-                    TitleWithImage(title: doctor.educationTypeLabel?.name ?? "Нет данных об образовании", imageName: "graduationcap")
-                    TitleWithImage(title: "Детская клиника \"РебёнОК\"", imageName: "mappin.and.ellipse")
+                photoAndNameBlock
+                
+                characteristics
                     
-                    NavigationLink(destination: AllPriceView(doctor: doctor).toolbarRole(.editor)) {
-                        PriceView(description: "Стоимость услуг", price: findMinimumPrice() ?? 0)
-                    }
-                    
-                    Text("Проводит диагностику и лечение терапевтических больных. Осуществляет расшифровку и снятие Экг. Дает рекомендации по диетологии. Доктор имеет опыт работы в России и зарубежом. Проводит консультации пациентов на английском языке")
-                        .font(.system(size: 14))
-                } .padding(16)
+                
+                PriceView(description: "Стоимость услуг", price: findMinimumPrice() ?? 0)
+                
+                description
                 
                 Spacer()
                 
-                Button(action: {
-                    print("Make an apointment")
-                }) {
+                NavigationLink(destination: AllPriceView(doctor: doctor).toolbarRole(.editor)) {
                     DoctorButtonLabel(buttonTitle: "Записаться")
                 }
+                .padding(.bottom, 10)
             }
+            .padding(.horizontal, 16)
             .navigationBarTitle("Педиатр", displayMode: .inline)
-            .padding(16)
-        })
+        }
     }
     
     func findMinimumPrice() -> Int? {
@@ -76,17 +45,61 @@ struct DoctorDetailView: View {
     }
     
     func chechScientificDegreeLabel() -> String {
-        if doctor.scientificDegreeLabel == "нет" {
+        if doctor.scientificDegreeLabel.rawValue == "нет" {
             return "Нет данных об ученой степени"
         } else {
-            return doctor.scientificDegreeLabel.capitalizedSentence
+            return doctor.scientificDegreeLabel.rawValue.capitalizedSentence
         }
     }
 }
 
 #Preview {
-    DoctorDetailView(doctor: Doctor.mockDoc)
+    DoctorDetailView(doctor: Responce.mockDoctor)
 }
 
 
 
+extension DoctorDetailView {
+    var characteristics: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            TitleWithImage(title: doctor.seniority.getYearsStringDetail(), imageName: "clock")
+            TitleWithImage(title: chechScientificDegreeLabel(), imageName: "cross.case")
+            TitleWithImage(title: doctor.educationTypeLabel?.name ?? "Нет данных об образовании", imageName: "graduationcap")
+            TitleWithImage(title: "Детская клиника \"РебёнОК\"", imageName: "mappin.and.ellipse")
+        }
+    }
+    
+    var description: some View {
+        Text("Проводит диагностику и лечение терапевтических больных. Осуществляет расшифровку и снятие Экг. Дает рекомендации по диетологии. Доктор имеет опыт работы в России и зарубежом. Проводит консультации пациентов на английском языке")
+            .font(.system(size: 14))
+    }
+    
+    var photoAndNameBlock: some View {
+        HStack(spacing: 16) {
+                AsyncImage(url: URL(string: doctor.avatar ?? "")) { phase in
+                    switch phase {
+                    case .empty:
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.accent)
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    default:
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.accent)
+                    }
+                
+            }
+            
+            Text("\(doctor.lastName)\n\(doctor.firstName) \(doctor.patronymic)")
+                .font(.system(size: 16, weight: .semibold))
+        }
+    }
+}
